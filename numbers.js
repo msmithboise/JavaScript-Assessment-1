93bb4832a364cc158258a20956df2094bb21d5a6
Michael Smith <msmithboise@gmail.com>
2018-08-02 16:29:17 -0600

    more pushes

--- numbers.js
+++ numbers.js
@@ -2,8 +2,21 @@
 var num10 = 10;
 var string8 = '8';
 var one = 1;
+
+
+var newNum = Number(string8);
+var answer = newNum += one += num10;
+
+console.log(answer)
+
+
 //2. write a loop that will log only numbers divisible by 3 between 20 - 100
 
+for (let i = 20; i < 100; i++) {
+    if (i % 3 == 0){
+      console.log(i)
+    }
+}
 
 
 //3. find the class average given the following test scores
